ebecabaddc79e58ce22960351f073c8a92bb792a
Anastasiia <blacsharm@gmail.com>
2024-05-02 19:43:04 +0300

    fix

--- src/API/creditsApi.js
+++ src/API/creditsApi.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 const apiKey = '19e9c4b8192481007639a2f9632cbc71';
 
-const creditsUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits`;
+const creditsUrl = `https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US`;
 const options = {
     params: {
         api_key: apiKey
     }
 };
 
-export const creditsMovie = async () => {
+export const creditsMovie = async (movieId) => {
     try {
-        const response = await axios.get(creditsUrl, options);
+        const response = await axios.get(creditsUrl, options, movieId);
         return response.data.results;
     } catch (error) {
         console.error(error);
