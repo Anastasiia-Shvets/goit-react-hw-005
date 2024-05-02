f8beba46ef925f3f8fe8a0a43fe9d3c4b58018be
Anastasiia <blacsharm@gmail.com>
2024-05-02 19:30:33 +0300

    upd

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ movies }) => {
     const location = useLocation()
     return (
         <ul className={style.list}>
-            {movies.map((movie) => (
+            {movies && movies.map((movie) => (
                 <li key={movie.id}>
                     <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>{movie.title}</NavLink>
                 </li>
