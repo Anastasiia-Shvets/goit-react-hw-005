75ebd67e744ca686f1dbf3ea719492adf05db375
Anastasiia <blacsharm@gmail.com>
2024-05-06 17:21:16 +0300

    pushCorrection

--- src/API/searchApi.js
+++ src/API/searchApi.js
@@ -7,7 +7,7 @@ const options = {
 };
 
 export const searchMovie = async (query) => {
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}include_adult=false&language=en-US&page=1`;
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
     try {
         const response = await axios.get(searchUrl, options);
         console.log(response.data.results);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,20 @@
-import { NavLink, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import style from './MovieList.module.css'
 
 const MovieList = ({ movies }) => {
-    const location = useLocation()
+    if (!movies) {
+        return null;
+    }
+    const location = useLocation();
     return (
         <ul className={style.list}>
-            {movies && movies.map((movie) => (
+            {movies.map((movie) => (
                 <li key={movie.id} className={style.item}>
-                    <NavLink to={`/movies/${movie.id}`} state={{ from: location }} className={style.link}>{movie.title}
+                    <Link state={location}
+                        to={`/movies/${movie.id}`}
+                        className={style.link}>{movie.title}
                         <img src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt={movie.title} />
-                    </NavLink>
+                    </Link>
                 </li>
             )
             )}
